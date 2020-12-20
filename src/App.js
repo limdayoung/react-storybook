adb4430ae40f441de6177cf4a1681e1f8925d6a0
limdayoung <dlaekdud625@gmail.com>
2020-12-21 01:16:58 +0900

    [initail commit]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TaskList from "./components/TaskList.js";
+
+const onPinTask = () => {
+  console.log("click onPinTask");
+};
+const onArchiveTask = () => {
+  console.log("click onArchiveTask");
+};
 
 function App() {
+  const tasks_sample = [
+    {
+      id: "1",
+      title: "Task 1",
+      state: "TASK_INBOX",
+    },
+    {
+      id: "2",
+      title: "Task 2",
+      state: "TASK_INBOX",
+    },
+    {
+      id: "3",
+      title: "Task 3",
+      state: "TASK_INBOX",
+    },
+    {
+      id: "4",
+      title: "Task 4",
+      state: "TASK_INBOX",
+    },
+    {
+      id: "5",
+      title: "Task 5",
+      state: "TASK_INBOX",
+    },
+    {
+      id: "6",
+      title: "Task 6 (pinned)",
+      state: "TASK_PINNED",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2 style={{ color: "#fff" }}>Learn storybook tutorial</h2>
+      <div style={{ padding: "3rem" }}>
+        <TaskList
+          // loading
+          tasks={tasks_sample}
+          onPinTask={onPinTask}
+          onArchiveTask={onArchiveTask}
+        />
+      </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders 'Learn storybook tutorial' link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn storybook tutorial/i);
   expect(linkElement).toBeInTheDocument();
 });
