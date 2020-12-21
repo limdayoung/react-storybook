1a55cebd6caff68e90994e8230e6ee3d98f36f2f
limdayoung <dlaekdud625@gmail.com>
2020-12-21 11:36:34 +0900

    GitHub action setup

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ React를 위한 Storybook 튜토리얼
 
 ### information
 1. start: yarn storybook
+
+### chromatic을 통한 배포
+- chromatic: https://www.chromatic.com/setup?appId=5fe0075e490b2d0021f384f3
+- storybook: https://5fe0075e490b2d0021f384f3-vvjylfivph.chromatic.com/?path=/story/inboxscreen--default
--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
 import React from "react";
-import "./App.css";
-import TaskList from "./components/TaskList.js";
+import { Provider } from "react-redux";
+import store from "./lib/redux";
 
-const onPinTask = () => {
-  console.log("click onPinTask");
-};
-const onArchiveTask = () => {
-  console.log("click onArchiveTask");
-};
+import InboxScreen from "./components/InboxScreen";
 
+import "./index.css";
 function App() {
-  const tasks_sample = [
-    {
-      id: "1",
-      title: "Task 1",
-      state: "TASK_INBOX",
-    },
-    {
-      id: "2",
-      title: "Task 2",
-      state: "TASK_INBOX",
-    },
-    {
-      id: "3",
-      title: "Task 3",
-      state: "TASK_INBOX",
-    },
-    {
-      id: "4",
-      title: "Task 4",
-      state: "TASK_INBOX",
-    },
-    {
-      id: "5",
-      title: "Task 5",
-      state: "TASK_INBOX",
-    },
-    {
-      id: "6",
-      title: "Task 6 (pinned)",
-      state: "TASK_PINNED",
-    },
-  ];
-
   return (
-    <>
-      <h2 style={{ color: "#fff" }}>Learn storybook tutorial</h2>
-      <div style={{ padding: "3rem" }}>
-        <TaskList
-          // loading
-          tasks={tasks_sample}
-          onPinTask={onPinTask}
-          onArchiveTask={onArchiveTask}
-        />
-      </div>
-    </>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   );
 }
-
 export default App;
